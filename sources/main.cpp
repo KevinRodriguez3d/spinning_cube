60ac4aa576645ddc00d605b9f75c135e8a9b7b10
Kevin Rodriguez <kevinrodriguez3d@gmail.com>
2025-09-28 14:22:12 -0700

    3D Spinning cube with slider control

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,44 +1,68 @@
 #include "raylib.h"
+#include "rlgl.h"
 
-int main(void)
-{
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 1080;
-    const int screenHeight = 720;
+#define RAYGUI_IMPLEMENTATION // define once
+#include "raygui.h"
 
-    SetConfigFlags(FLAG_VSYNC_HINT);
+int main(void) {
+  // Initialization
+  //--------------------------------------------------------------------------------------
+  SetConfigFlags(FLAG_VSYNC_HINT);
+  SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+  SetConfigFlags(FLAG_MSAA_4X_HINT); // anti-aliasing
+  const int screenWidth = 1200;
+  const int screenHeight = 900;
+  SetTargetFPS(60);
+  InitWindow(screenWidth, screenHeight, "Spinning_Cube");
 
-    InitWindow(screenWidth, screenHeight, "raylib template");
+  // Camera set up
+  Camera3D camera = {0};
+  camera.position = (Vector3){0, 10, 10};
+  camera.target = (Vector3){0, 0, 0};
+  camera.up = (Vector3){0, 1, 0};
+  camera.fovy = 60.0f;
+  camera.projection = CAMERA_PERSPECTIVE;
 
-    SetTargetFPS(60);
-    //--------------------------------------------------------------------------------------
+  // cube parameters
+  Vector3 cubePosition = {0, 0, 0};
+  float cubeRotation = {0.0};
+  float rotationSpeed = {0};
 
-    // Main game loop
-    while (!WindowShouldClose())
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        
-        //----------------------------------------------------------------------------------
+  //--------------------------------------------------------------------------------------
+  // Main game loop
+  while (!WindowShouldClose()) {
+    // Update
+    //----------------------------------------------------------------------------------
+    cubeRotation += (5.0 * rotationSpeed);
+    //----------------------------------------------------------------------------------
 
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
+    // Draw
+    //----------------------------------------------------------------------------------
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    BeginMode3D(camera);
 
-            ClearBackground(RAYWHITE);
-            DrawFPS(10,10);
+        rlPushMatrix();
+            rlRotatef(cubeRotation, 0, 1, 0);
+            DrawCube(cubePosition, 2, 2, 2, GRAY);
+            DrawCubeWires(cubePosition, 2, 2, 2, BLACK);
+        rlPopMatrix();
 
-            DrawText("WINDOW!", screenWidth/2 - 50, screenHeight/2, 100, RED);
+        DrawGrid(10, 1);
 
-        EndDrawing();
-        //----------------------------------------------------------------------------------
-    }
+    EndMode3D();
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();
-    //--------------------------------------------------------------------------------------
+    DrawFPS(10, 10);
 
-    return 0;
+    // slider
+    GuiSlider((Rectangle){(float)(GetScreenWidth()/2.0)-150, ((float)GetScreenHeight()-60), 300, 10}, "0", "5", &rotationSpeed, 0.0, 5.0);
+
+    EndDrawing();
+    //----------------------------------------------------------------------------------
+  }
+  // De-Initialization
+  //--------------------------------------------------------------------------------------
+  CloseWindow();
+  //--------------------------------------------------------------------------------------
+  return 0;
 }
