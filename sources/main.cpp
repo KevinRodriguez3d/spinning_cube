280fffd30b692c43fb214db748d74d3631b8b2d5
Kevin Rodriguez <kevinrodriguez3d@gmail.com>
2025-09-30 23:17:30 -0700

    Add rotation axis controls

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,5 +1,6 @@
 #include "raylib.h"
 #include "rlgl.h"
+#include "raymath.h"
 
 #define RAYGUI_IMPLEMENTATION // define once
 #include "raygui.h"
@@ -25,15 +26,21 @@ int main(void) {
 
   // Cube parameters
   Vector3 cubePosition = {0, 0, 0};
+  Vector3 cubeRotAxis = {0,0,0};
   float cubeRotation = {0.0};
   float rotationSpeed = {0};
   Color cubeColor = GRAY;
+  Color cubeAxisColor = BLACK;
 
   // UI parameters
   int activeDropDown {0};
   bool dropEdit {false};
+  bool xAxis, yAxis, zAxis {false};
+  int axis {0};
 
   GuiSetStyle(DROPDOWNBOX, TEXT_SIZE, 30);
+  GuiSetStyle(CHECKBOX, BORDER_COLOR_NORMAL,ColorToInt(WHITE));
+  GuiSetStyle(CHECKBOX, TEXT_COLOR_FOCUSED,ColorToInt(WHITE));
 
   //------------------------------------------------------------------------------
   // Main game loop
@@ -69,6 +76,21 @@ int main(void) {
             cubeColor = VIOLET;
             break;
     }
+
+    switch (axis) {
+        case 0:
+            cubeRotAxis = {1,0,0};
+            cubeAxisColor = RED;
+            break;
+        case 1:
+            cubeRotAxis = {0,1,0};
+            cubeAxisColor = GREEN;
+            break;
+        case 2:
+            cubeRotAxis = {0,0,1};
+            cubeAxisColor = BLUE;
+            break;
+    }
     //------------------------------------------------------------------------------
 
     // Draw
@@ -83,19 +105,25 @@ int main(void) {
     BeginMode3D(camera);
 
         rlPushMatrix();
-            rlRotatef(cubeRotation, 0, 1, 0);
+            rlRotatef(cubeRotation, cubeRotAxis.x, cubeRotAxis.y, cubeRotAxis.z );
             DrawCube(cubePosition, 2, 2, 2, cubeColor);
             DrawCubeWires(cubePosition, 2, 2, 2, BLACK);
         rlPopMatrix();
 
         DrawGrid(10, 1);
 
+        DrawLine3D({0,0,0}, Vector3Scale(cubeRotAxis, 5), cubeAxisColor);
+
     EndMode3D();
 
     DrawFPS(10, 10);
 
-    GuiSlider((Rectangle){(float)(GetScreenWidth()/2.0)-400, ((float)GetScreenHeight()-50), 800, 30}, "0", "1", &rotationSpeed, 0.0, 600.0);
 
+    if(GuiCheckBox({10,200,20,20}, "X", &xAxis)) axis = 0;
+    if(GuiCheckBox({50,200,20,20}, "Y", &yAxis)) axis = 1;
+    if(GuiCheckBox({90,200,20,20}, "Z", &zAxis)) axis = 2;
+
+    GuiSlider((Rectangle){(float)(GetScreenWidth()/2.0)-400, ((float)GetScreenHeight()-50), 800, 30}, "0", "1", &rotationSpeed, 0.0, 600.0);
     if(GuiDropdownBox({(float)(GetScreenWidth()/2.0)+250,50,200,50}, "Gray;Red;Green;Blue;Purple;Beige;Brown;Violet", &activeDropDown, dropEdit)) dropEdit = !dropEdit;
 
     EndDrawing();
